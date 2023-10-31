7f0d7683f2a2a61bb5c7e5f339a69bcddbc725ea
znehraks <znehraks@gmail.com>
2023-10-31 18:26:27 +0900

    aws 배포

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      xzlkjslkjf
+      xzlkjslkjfasjdkasl
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -23,7 +23,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> aslkdjaskldjas and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
